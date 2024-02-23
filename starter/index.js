e0a263d7c5b80728c823cc4ed72b8d4db37431fc
Jean-003 <amasterpiece02@gmail.com>
2024-02-23 20:15:30 +0000

    added inquirer prompt functions for each employee

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,114 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+function createTeam () {
+      inquirer
+    .prompt([
+        {
+        type: 'list',
+        name: 'employee type',
+        message: 'Add another employee?',
+        choices: ['Engineer', 'Intern'], 
+    },
+    ])
+ }
+
+function createManager () {
+
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: "Manager's name?"
+      
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is their employee ID?',
+        
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is their email address?',
+        
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is their office number?',
+        
+    },  
+  ])
+
+}
+
+function createEngineer () {
+
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: "Engineer's name?"
+      
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is their employee ID?',
+        
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is their email address?',
+        
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is their office number?',     
+  },
+])
+
+}
+function createIntern () {
+
+    inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: "Intern's name?"
+      
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is their employee ID?',
+        
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is their email address?',
+        
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is their office number?',
+        
+    },
+  
+  ])
+
+}
+
+promptUser()
+.then((answers) => writeFileAsync('index.html', generateHTML(answers)))
+.then(() => console.log('Successfully wrote to index.html'))
+.catch((err) => console.error(err));
